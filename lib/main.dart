4d12256fe843aab852cdd0cebe997d10daf50acb
nischalshk <nischal.shk@gmail.com>
2021-04-11 13:32:18 +0545

    orders visible, app drawer added.

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:shop_state/providers/cart.dart';
 import 'package:shop_state/providers/orders.dart';
 import 'package:shop_state/providers/products.dart';
 import 'package:shop_state/screens/cart_screen.dart';
+import 'package:shop_state/screens/orders_screen.dart';
 import 'package:shop_state/screens/product_detail_screen.dart';
 import 'package:shop_state/screens/products_overview_screen.dart';
 
@@ -33,6 +34,9 @@ ChangeNotifierProvider(create:(ctx)=> Orders(),),
           routes: {
             ProductDetailScreen.routeName: (ctx)=>ProductDetailScreen(),
             CartScreen.routeName:(ctx)=>CartScreen(),
+            OrdersScreen.routeName:(ctx)=>OrdersScreen(),
+         
+         
           }
           
           
--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop_state/providers/orders.dart' show Orders;
+import 'package:shop_state/widgets/app_drawer.dart';
 import 'package:shop_state/widgets/order_item.dart';
 
 class OrdersScreen extends StatelessWidget {
+  static const routeName = '/orders';
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -13,6 +15,7 @@ class OrdersScreen extends StatelessWidget {
         appBar: AppBar(
           title: Text('Your Orders'),
         ),
+        drawer:AppDrawer(),
         body: ListView.builder(
           itemBuilder: (context, index)=>OrderItem(orderData.orders[index]),
           itemCount: orderData.orders.length,
